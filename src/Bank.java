aaa4c83259395c9cfcbe4591b52d41dfada65be5
wignn <tigfiragnafatur1933@gmail.com>
2024-08-31 12:21:05 +0700

    two

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+class Bank {
+    int Saldo;
 
-public class Bank {
+    public Bank(int Saldo) {
+        this.Saldo = Saldo;
+        System.out.println("selamat datang");
+    }
+    public void tampilkanSaldo() {
+        System.out.println("Saldo saat ini: " + Saldo + "\n");
+    }
+    void simpanUang(int Simpan) {
+        this.Saldo += Simpan;
+        System.out.println("Simpan uang: " + Simpan);
+        tampilkanSaldo();
+    }
+    void ambilUang(int Ambil) {
+        this.Saldo -= Ambil;
+        System.out.println("Ambil uang: " + Saldo);
+        tampilkanSaldo();
+    }
 }
--- src/BankBeraksi.java
+++ src/BankBeraksi.java
@@ -1,29 +1,5 @@
-class Bank {
-    int Saldo;
 
-    public Bank(int Saldo) {
-        this.Saldo = Saldo;
-        System.out.println("selamat datang");
-    }
-
-    public void tampilkanSaldo() {
-        System.out.println("Saldo saat ini: " + Saldo + "\n");
-    }
-
-    void simpanUang(int Simpan) {
-        this.Saldo += Simpan;
-        System.out.println("Simpan uang: " + Simpan);
-        tampilkanSaldo();
-    }
-
-     void ambilUang(int Ambil) {
-        this.Saldo -= Ambil;
-        System.out.println("Ambil uang: " + Saldo);
-        tampilkanSaldo();
-    }
-}
-
-public class Main {
+public class BankBeraksi {
     public static void main(String[] args) {
         Bank rekening = new Bank(100000);
         rekening.tampilkanSaldo();
